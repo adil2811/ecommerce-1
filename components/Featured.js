c07228eb145c5876f1282335e2847b991221427a
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-08-09 01:43:48 +0530

    Update Featured.js

--- components/Featured.js
+++ components/Featured.js
@@ -71,8 +71,8 @@ export default function Featured({product}) {
         <p className="mt-4 mb-8 max-w-md text-gray-500">{product.description}</p>
         <button onClick={addFeaturedToCart}  className="group mt-auto flex w-44 cursor-pointer select-none items-center justify-center rounded-md bg-black px-6 py-2 text-white transition">
           <span  className="group flex w-full items-center justify-center rounded py-1 text-center font-bold"> Add to cart </span>
-          <svg className="flex-0 group-hover:w-6 ml-4 h-6 w-0 transition-all" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+          <svg className="flex-0 group-hover:w-6 ml-4 h-6 w-0 transition-all" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
           </svg>
         </button>
       </div>
