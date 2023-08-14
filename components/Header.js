feacb39a4271b1d19d301624d87b34efc3d66602
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-08-14 20:33:07 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -223,9 +223,7 @@ className="rounded-full"
 <li>
 <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</Link>
 </li>
-<li>
-<Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</Link>
-</li>
+
 </ul>
 <div className="py-2">
 <span  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
