16b5b5c8f2fec81fab74d4adda6594427a6ed9de
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-08-14 13:07:43 +0530

    Update List.js

--- components/List.js
+++ components/List.js
@@ -10,21 +10,22 @@ export default function List({price,images,title,_id}) {
   <div key={_id} className=''>
    
 
-<div  key={_id} className="p-1 mt-2  w-[300px] max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 inline-block justify-between	mr-2 ml-2  ">
+<div  key={_id} className=" p-1 mt-4  w-[250px] max-sm:w-[120px]  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 inline-block justify-between	mr-2 ml-2  ">
     <Link href="#">
-        <img className="rounded-lg ml-3 w-[250px] h-[200px]  " src={images[0]} alt="product image" />
+        <img className="rounded-lg ml-3  w-[200px] max-sm:w-[90px] h-[150px] max-sm:h-[80px]  " src={images[0]} alt="product image" />
     </Link>
     <div className="px-0 pb-0 ">
         <Link href="#">
-          <span className='text-[15px] text-gray-500 ml-2 p-1'>Apple Laptops </span>
-            <h5 className="flex text-[18px] font-semibold tracking-tight text-gray-900 dark:text-white ml-3">{title} <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="mt-[6px] ml-[1px] text-white-400 w-4 h-4">
+          <span className='text-[13px] max-sm:text-[9px] text-gray-500 ml-2 p-1'>Apple Laptops </span>
+            <h5 className="flex text-[15px] max-sm:text-[9px] font-semibold tracking-tight text-gray-900 dark:text-white ml-3">{title} 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="mt-[6px] max-sm:mt-[0px] ml-[4px] text-white-400 w-4 max-sm:w-[10px] h-4 ">
   <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
 </svg>
 </h5>
-                        <span className="text-3xl font-bold text-gray-900 dark:text-white ml-3">₹{price}</span>
+                        <span className="text-2xl max-sm:text-[15px]  font-bold text-gray-900 dark:text-white ml-3">₹{price}</span>
 
         </Link>
-        <div className="flex items-center mt-2.5 mb-5">
+        <div className="flex items-center mt-2.5 max-sm:mt-[0px] mb-5 max-sm: mt-0">
             <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
             </svg>
@@ -43,7 +44,7 @@ export default function List({price,images,title,_id}) {
             <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">4.0</span>
         </div>
         <div className="flex pb-0 ">
-            <Link href="#" className="h-[50px] text-white bg-black-800 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium text-lg rounded-[4px]  text-sm px-5 py-2.5 text-center dark:bg-red-800 dark:hover:bg-red-600 dark:focus:ring-red-800 w-full">Add to cart</Link>
+            <Link href="#" className="h-[50px]  max-sm:h-[40px] text-white bg-red-800 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium text-lg rounded-[4px]  text-sm max-sm:text-[11px] px-5 py-2.5 text-center dark:bg-red-800 dark:hover:bg-red-600 dark:focus:ring-red-800 w-full">Add to cart</Link>
         </div>
     </div>
 </div>
