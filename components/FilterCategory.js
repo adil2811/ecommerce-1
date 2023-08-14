de8e2903728ada99c792c392cdf0f5a20c56aa84
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-08-14 13:07:35 +0530

    Update FilterCategory.js

--- components/FilterCategory.js
+++ components/FilterCategory.js
@@ -1,12 +1,15 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useFilterContext } from './Filter_context';
 
 export default function   FilterCategory() {
 
     const { categories , name,updateFilterValue} = useFilterContext();
 
- console.log(categories)
+
+ 
+ 
+    console.log(categories)
 
 //  const getUniqueData = (data,property,_id) => {
 //     let newVal = data.map((curElem)=>{
@@ -23,13 +26,13 @@ export default function   FilterCategory() {
   return (
 
     <>
-    <h3 className='mt-[13px] ml-9 text-black-900 text-xl' >
+    <h3 className='mt-[13px] ml-9 max-sm:ml-[4px] text-black-900 text-xl max-sm:text-[15px]' >
        Category
     </h3>
-    <ul className='ml-8 p-2 '  >
-        <li className='text-gray-600 hover:text-green-600'><button type="button"  name='category' onClick={updateFilterValue}>All</button></li>
+    <ul className='ml-8 max-sm:ml-[0px] p-2 text-left	  '  >
+        <li className='text-gray-600 hover:text-green-600 max-sm:text-[8px]'><button type="button"  name='category' onClick={updateFilterValue}>All</button></li>
    {categories.map((curElem,index)=> {
-        return  <li className='mt-1 text-gray-600 hover:text-green-600 active:text-red-600 ' key={index}><button key={index} type="button" value={curElem._id} name='category' onClick={updateFilterValue}>{curElem.name}</button></li>
+        return  <li className='max-sm:text-[8px] mt-1 text-gray-600 hover:text-green-600 active:text-red-600 ' key={index}><button key={index} type="button" value={curElem._id} name='category' onClick={updateFilterValue}>{curElem.name}</button></li>
     })
 
    }
