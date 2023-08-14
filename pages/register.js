d9259a57f4539afa0bdb71f7a3c25f3d45fa9c2f
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-08-14 13:07:52 +0530

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -67,7 +67,7 @@ const Register = () => {
          onChange={(e) => setPassword(e.target.value)}
            className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
       </div>
-      <button className="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
+      <button className="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Register</button>
       <p className="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
     </div>
     
