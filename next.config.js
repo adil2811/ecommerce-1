cfee5bc9cd45a4ca0be2a342e8e879481359296f
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-08-21 18:32:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,47 +6,18 @@ const nextConfig = {
   },
 }
  
-module.exports = { 
-  nextConfig,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'adil-next-ecommerce.s3.amazonaws.com',
-        port: '443',
-        pathname: '/1689169977534.jpg',
-      },
-    ],
-    // limit of 25 deviceSizes values
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    // limit of 25 imageSizes values
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    // limit of 50 domains values
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-
-
-
-    loader: 'default',
-    // file with `export default function loader({src, width, quality})`
-    loaderFile: '',
-    // disable static imports for image files
-    disableStaticImages: false,
-    // minimumCacheTTL is in seconds, must be integer 0 or more
-    minimumCacheTTL: 60,
-    // ordered list of acceptable optimized image formats (mime types)
-    formats: ['image/webp'],
-    // enable dangerous use of SVG images
-    dangerouslyAllowSVG: false,
-    // set the Content-Security-Policy header
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    // sets the Content-Disposition header (inline or attachment)
-    contentDispositionType: 'inline',
-    // limit of 50 objects
-    remotePatterns: [],
-    // when true, every image will be unoptimized
-    unoptimized: false,
+    return config;
   },
-}
+};
+
 
 
 
