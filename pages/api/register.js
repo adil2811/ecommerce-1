d28d8ea063233800993e21d99d27f5ba2cc4019d
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-08-21 18:33:13 +0530

    Update register.js

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,16 +1,41 @@
-import React from 'react'
 import User from '@/models/User';
-import {mongooseConnect} from "@/lib/mongoose";
+import { mongooseConnect } from '@/lib/mongoose';
+import Wishlist from '@/models/Wishlist';
 
-export default async function handler(req,res) {
-    if(req.method === 'POST'){
-    await mongooseConnect();
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      await mongooseConnect();
 
-    const {name , email ,password} = req.body
 
-    const user = await User.create({ name, email , password})
+      const { name, email, password, wishlist } = req.body;
 
-    res.status(201).json({ user })
+      const userExists = await User.findOne({ email });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+
+      // Create wishlist items and get their IDs
+      const Wishlists = await Promise.all(
+        wishlist.map(async (item) => {
+          const createdItem = await Wishlist.create({ name: item });
+          return createdItem._id;
+        })
+      );
+
+      // Create the user with the associated wishlist items
+      const user = await User.create({
+        name,
+        email,
+        password,
+        wishlist: Wishlists, // Associate wishlist items with the user
+      });
+
+      res.status(201).json({ user });
+    } catch (error) {
+      console.error('Error creating user:', error);
+      res.status(500).json({ error: 'Server error' });
     }
-
+  }
 }
