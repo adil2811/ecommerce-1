9cc31fd7e010f699210c793add69b014753cc863
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-09-26 20:55:04 +0530

    mc sttan

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,46 +1,37 @@
-import { mongooseConnect } from "@/lib/mongoose";
+// pages/api/webhook.js
+
 import { buffer } from 'micro';
-import { Order } from "@/models/Order";
-import Stripe from 'stripe';
+import { stripe } from '@/utils/stripe';
+import { endpointSecret } from '@/config';
 
-const stripe = new Stripe(process.env.STRIPE_SK, {
-  apiVersion: '2020-08-27', // Specify the Stripe API version you are using
-});
-
-const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
-
-export default async function handler(req, res) {
-  try {
-    await mongooseConnect(); // Connect to the database
-
-    const sig = req.headers['stripe-signature'];
-    const event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret);
-
-    // Handle the event
-    switch (event.type) {
-      case 'checkout.session.completed':
-        const data = event.data.object;
-        const orderId = data.metadata.orderId;
-        const paid = data.payment_status === 'paid';
-
-        if (orderId && paid) {
-          // Update the order as paid in your database
-          await Order.findByIdAndUpdate(orderId, { paid: true });
-        }
-        break;
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-
-    res.status(200).send('ok');
-  } catch (err) {
-    console.error(`Webhook Error: ${err.message}`);
-    res.status(400).send(`Webhook Error: ${err.message}`);
+export default async (req,res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).end(); // Method Not Allowed
   }
-}
 
-export const config = {
-  api: {
-    bodyParser: false, // Disable built-in bodyParser to handle the request as a buffer
-  },
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    const buf = await buffer(req);
+    event = stripe.webhooks.constructEvent(buf, sig, Process.env.STRIPE_WEBHOOK_SECRET);
+  } catch (err) {
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === 'paid';
+      if (orderId && paid) {
+        // Update your order in the database here
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  return res.status(200).end();
 };
